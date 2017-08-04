5e8136531c1c066d671a5b66adae7736ea38851d
App Academy Student <student@appacademy.io>
2017-08-03 17:59:35 -0700

    Add populate_numbers and get_coordinate

--- minesweeper.rb
+++ minesweeper.rb
@@ -12,10 +12,37 @@ class MinesweeperGame
   end
 
   def populate_bombs
-    bomb_coordinates = @board.list_of_coordinates.sample(@bomb_count)
-    bomb_coordinates.each do |pos|
+    @bomb_coordinates = @board.list_of_coordinates.sample(@bomb_count)
+    @bomb_coordinates.each do |pos|
       @board[pos].value = '@'.black.on_red
     end
+  end
+
+  def populate_numbers
+    @bomb_coordinates.each do |bomb_pos|
+      p bomb_pos
+      adjacent_coordinates(bomb_pos).each do |adj_pos|
+        value = @board[adj_pos].value # is either @ or ' ' or digit string
+
+        @board[adj_pos].value = (value.uncolorize.to_i + 1).to_s.on_light_white unless value == '@'.black.on_red
+      end
+    end
+  end
+
+  def adjacent_coordinates(pos)
+    row, col = pos
+    result = []
+
+    result << [row - 1, col - 1] if row > 0 && col >= 0 # upleft
+    result << [row - 1, col] if row > 0 # up
+    result << [row - 1, col + 1] if row > 0 && @board[[row - 1, col + 1]] # upright
+    result << [row, col + 1 ] if @board[[row, col + 1]] # right
+    result << [row + 1, col + 1] if @board[[row + 1, col + 1]] # rightdown
+    result << [row + 1, col] if @board[[row + 1, col]] # down
+    result << [row + 1, col - 1] if col > 0 && @board[[row + 1, col - 1]] # downleft
+    result << [row, col - 1] if col > 0 # left
+
+    result
   end
 
   def play
@@ -23,7 +50,15 @@ class MinesweeperGame
   end
 
   def play_turn
-    puts ""
+
+    get_coordinate
+  end
+
+  private
+
+  def get_coordinate
+    puts 'Please enter coordinates (e.g. I,A for top-right)'
+    input = gets.chomp.split(/\,?\s*/).reverse
   end
 
   def game_over?
@@ -33,5 +68,6 @@ end
 if __FILE__ == $0
   g = MinesweeperGame.new
   g.populate_bombs
+  g.populate_numbers
   g.board.render
 end
